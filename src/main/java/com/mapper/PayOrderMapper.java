74e14f856719681c71cfc0a5e0af62057395107f
mynameabc <21654428@qq.com>
2019-12-31 23:40:23 +0800

    -

---
+++ src/main/java/com/mapper/PayOrderMapper.java
@@ -0,0 +1,16 @@
+package com.mapper;
+
+import com.MyMapper;
+import com.pojo.entity.PayOrder;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface PayOrderMapper extends MyMapper<PayOrder> {
+
+    PayOrder getOrderForPlatformOrderNo(@Param("platformOrderNo")String platformOrderNo);
+
+    List<PayOrder> getOrderForStatus(@Param("status")String status);
+
+    void setNumberIni();
+}
